39d7b00a5f6e2917a3d6bba3b029699942689fd3
Stian-Wolmarans <57462145+Stian-Wolmarans@users.noreply.github.com>
2021-02-01 20:12:41 +0000

    Create Palindrome.py

---
+++ Python Practice/Python.org Practice/Palindrome.py
@@ -0,0 +1,34 @@
+word = input("Enter a word: ")
+
+array = list(word)
+
+print(array)
+
+n = len(array)
+"""
+print(n)
+
+print(array[0])
+print(array[1])
+print(array[2])
+"""
+"""
+if array[0] == array[n-1]:
+    print("success")
+"""
+
+y = 0
+while(y < n):
+    if array[y] == array[n-1]:
+        x = 0
+        y = y + 1
+    else:
+        x = 1
+        y = n
+if x == 0:
+    print("Word is a palindrome")
+elif x ==1:
+    print("Word is not a palindrome")
+
+print("Done")
+
