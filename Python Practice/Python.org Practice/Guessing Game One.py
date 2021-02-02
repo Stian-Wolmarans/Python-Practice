b7f3d3ddd86743081564d753137502a51b7001d8
Stian-Wolmarans <57462145+Stian-Wolmarans@users.noreply.github.com>
2021-02-02 08:44:43 +0000

    Update Guessing Game One.py
    Added game score tally at end

--- Python Practice/Python.org Practice/Guessing Game One.py
+++ Python Practice/Python.org Practice/Guessing Game One.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 repeat = 1
+guesses = 0
+correct = 0
+incorrect = 0
 
 while(repeat == 1):
 
-    number = np.random.randint(1,9, size = 1)
+    number = np.random.randint(1,5, size = 1)
 
     #print(number)
 
@@ -15,6 +18,13 @@ while(repeat == 1):
     if number == usernum:
         print("You guessed the right number! Do you want to play again \n 1 For Yes \n 2 For No")
         repeat = int(input())
+        guesses += 1
+        correct += 1
     else:
         print("Your guessed the wrong number :( Do you want to play again \n 1 For Yes \n 2 For No")
         repeat = int(input())
+        guesses += 1
+        incorrect += 1
+print(" Giving up so soon? Here are your results: ")
+print(" Guesses = ", guesses, "\n Correct = ", correct, "\n Incorrect = ", incorrect)
+
